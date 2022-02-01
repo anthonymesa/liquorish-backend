0135eddbac3c2586c42b865d51c3bf0e62c444cc
JakeKeels <69876432+JakeKeels@users.noreply.github.com>
2022-01-31 20:28:26 -0500

    Added extra page test for GET requests

--- server.js
+++ server.js
@@ -22,39 +22,39 @@ let DB_PASSWORD = process.env.DB_PASSWORD;
 
 //  Configuration for database connection
 const db_config = {
-  server: DB_SERVER,
-  database: DB_DATABASE,
-  options: {
-    database: DB_DATABASE
-  },
-  authentication: {
-    type: "default",
+    server: DB_SERVER,
+    database: DB_DATABASE,
     options: {
-      userName: DB_USER_NAME,
-      password: DB_PASSWORD,
-      port: 1433
+        database: DB_DATABASE
+    },
+    authentication: {
+        type: "default",
+        options: {
+            userName: DB_USER_NAME,
+            password: DB_PASSWORD,
+            port: 1433
+        }
     }
-  }
 };
 
 function init_db() {
-  //  Create new database connection object using configuration
-  db_connection = new Connection(db_config);
+    //  Create new database connection object using configuration
+    db_connection = new Connection(db_config);
 
-  //  Set callback for db_connection's 'connect' event, to be called after 
-  //  trying to connect via .connect().
-  db_connection.on("connect", err => {
-    if (err) {
-      console.error(err);
-      exit(-1);
-    } else {
-      init_server(db_connection);
-      console.log("Database connected...");
-    }
-  });
+    //  Set callback for db_connection's 'connect' event, to be called after 
+    //  trying to connect via .connect().
+    db_connection.on("connect", err => {
+        if (err) {
+            console.error(err);
+            exit(-1);
+        } else {
+            init_server(db_connection);
+            console.log("Database connected...");
+        }
+    });
 
-  //  Try to establish a connection with the database.
-  db_connection.connect();
+    //  Try to establish a connection with the database.
+    db_connection.connect();
 }
 
 //=============================================================================
@@ -64,68 +64,90 @@ function init_db() {
 //  Define asynchronous function to initialize server with its routes defined.
 async function init_server(db_connection) {
 
-  //  Creates Hapi server object that uses the port given by environment variable.
-  //  if the environment variable doesn't exist, then 8080 is default.
-  const server = new Hapi.Server({
-    port: process.env.PORT || 8080,
-    state: {
-      strictHeader: false
-    },
-    routes: {
-      cors: true
-    }
-  });
+    //  Creates Hapi server object that uses the port given by environment variable.
+    //  if the environment variable doesn't exist, then 8080 is default.
+    const server = new Hapi.Server({
+        port: process.env.PORT || 8080,
+        state: {
+            strictHeader: false
+        },
+        routes: {
+            cors: true
+        }
+    });
 
-  //  Set the url routes that the API will serve.
-  set_routes(server, db_connection);
+    //  Set the url routes that the API will serve.
+    set_routes(server, db_connection);
 
-  await server.start();
+    await server.start();
 };
 
 //  Set the routes for the server that can be called as API.
-function set_routes(server, db_connection)
-{
-  //  Defines route callback for GET requests sent to 'http://.../test'.
-  //  Returns a promise to be fulfilled.
+function set_routes(server, db_connection) {
+    //  Defines route callback for GET requests sent to 'http://.../test'.
+    //  Returns a promise to be fulfilled.
 
-  //  API Function: test
-  //    Tests that the database is up and connected by sending
-  //    a query to evaluate the length of the test_table, which
-  //    should only have a single value in it. 
-  server.route({
-    method: 'GET',
-    path: '/test',
-    handler: function (request, reply) {
-      return new Promise((resolve, reject) => {
-        //  Create dabase request to count from test table (should be 1)
-        const request = new Request(`SELECT count(value) FROM test_table`,
-          (err, rowCount) => {
-            if (err) {
-              console.log(err);
-              resolve(false);
-            } else {
-              console.log(rowCount);
-              resolve(rowCount == 1);
-            }
-          }
-        );
-    
-        db_connection.execSql(request);
-      });
-    }
-  });
+    //  API Function: test
+    //    Tests that the database is up and connected by sending
+    //    a query to evaluate the length of the test_table, which
+    //    should only have a single value in it. 
+    server.route({
+        method: 'GET',
+        path: '/test',
+        handler: function (request, reply) {
+            return new Promise((resolve, reject) => {
+                //  Create dabase request to count from test table (should be 1)
+                const request = new Request(`SELECT count(value) FROM test_table`,
+                    (err, rowCount) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        } else {
+                            console.log(rowCount);
+                            resolve(rowCount == 1);
+                        }
+                    }
+                );
 
-  //  API Function: do nothing
-  //    This route catches all paths that are not explicitly given above.
-  //    therefore any call to a URL that isn't defined above will get the
-  //    'curved swords' message.
-  server.route({
-    method: 'GET',
-    path: '/{path*}',
-    handler: function (request, reply) {
-      return "They have curved swords!";
-    }
-  });
+                db_connection.execSql(request);
+            });
+        }
+    });
+
+    //  API Function: do nothing
+    //    This route catches all paths that are not explicitly given above.
+    //    therefore any call to a URL that isn't defined above will get the
+    //    'curved swords' message.
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: function (request, reply) {
+            return "They have curved swords!";
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/testTables'
+        handler: function (request, reply) {
+            return new Promise((resolve, reject) => {
+                //  Create dabase request to count from test table (should be 1)
+                const request = new Request(`SELECT * FROM bar`,
+                    (err, table) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        } else {
+                            console.log(table);
+                            resolve(table);
+                        }
+                    }
+                );
+
+                db_connection.execSql(request);
+            });
+        }
+    });
 }
 
 //=============================================================================
