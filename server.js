50782044ef6a49c7e26f53306ae442f39e0666f8
Zoharr2022 <89365408+Zoharr2022@users.noreply.github.com>
2022-02-27 17:05:26 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -13,24 +13,13 @@ const { exit } = require("process");
 //=============================================================================
 //  DATABASE
 //=============================================================================
-/*
- Server=tcp:liquorish-dbs.database.windows.net,1433;Initial Catalog=liquorish-db;
- Persist Security Info=False;User ID=LorisMoris;Password={your_password};
- MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
- 
- let DB_SERVER = process.env.DB_SERVER;
+
+//  Get database info from environment variables
+let DB_SERVER = process.env.DB_SERVER;
 let DB_DATABASE = process.env.DB_DATABASE;
 let DB_USER_NAME = process.env.DB_USER_NAME;
 let DB_PASSWORD = process.env.DB_PASSWORD;
- */  
 
-//Get database info from environment variables
-let DB_SERVER = 'liquorish-dbs.database.windows.net';
-let DB_DATABASE = 'liquorish-db';
-let DB_USER_NAME ='LorisMoris';
-let DB_PASSWORD = 'CanadianSparkle3';
-
-//console.log("DB_SERVER", DB_SERVER);
 //  Configuration for database connection
 const db_config = {
     server: DB_SERVER,
@@ -71,12 +60,13 @@ function init_db() {
 //=============================================================================
 //  SERVER
 //=============================================================================
-
+const PORT =  process.env.PORT || 8080;
 //  Define asynchronous function to initialize server with its routes defined.
 async function init_server(db_connection) {
 
     //  Creates Hapi server object that uses the port given by environment variable.
     //  if the environment variable doesn't exist, then 8080 is default.
+    console.log("starting on port: "+PORT);
     const server = new Hapi.Server({
         port: process.env.PORT || 8080,
         state: {
@@ -106,7 +96,6 @@ function set_routes(server, db_connection) {
         method: 'GET',
         path: '/test',
         handler: function (request, reply) {
-            console.log("TEST");
             return new Promise((resolve, reject) => {
                 //  Create dabase request to count from test table (should be 1)
                 const request = new Request(`SELECT count(value) FROM test_table`,
@@ -158,7 +147,7 @@ function set_routes(server, db_connection) {
         //  Then username would equate 'JoJo217' and password_hash 'EDDEF9E8E578C2A560C3187C4152C8B6F3F90C1DCF8C88B386AC1A9A96079C2C'
         //  (the actual password for this dummy account is 'TestPass' which has been run through sha256 encryption via https://passwordsgenerator.net/sha256-hash-generator/
         //  though we will need to be sha'ing ourselves on the front end).
-        path: '/login/{username}/{password_hash}',
+        path: '/login/{username}/{password}',
 
         //  This fucntion is async so that we can await the database call synchronously
         handler: async function (request, reply) {
@@ -167,12 +156,13 @@ function set_routes(server, db_connection) {
             //  of scope inside the promise (because the request object
             //  may not be available at the point we are trying to access the 
             //  values in the promise due to the asynchronous nature of things).
+
             const username = request.params.username;
-            const password_hash = request.params.password_hash;
+            const password = request.params.password;
             
             //  declare the password hash variable so that it can be filled inside the request.
-            let true_password_hash;
-
+            let true_password;
+            let userId=-1;
             //  because we have to wait on the response from the databse, we can call await
             //  to ensure that we synchronously make our databse call before returning the 
             //  data to the connection that requested it.
@@ -182,7 +172,7 @@ function set_routes(server, db_connection) {
                 //  and then get the pass hash from the user_pass table given
                 //  the user id. there should either be 1 row return, or 0.
                 const request = new Request(
-                    `select password_hash from users_pass where users_id = (
+                    `select password, users_id from users_pass where users_id = (
                         select id from users where username = '${username}'
                     )`,
                     (err, rowCount) => {
@@ -203,7 +193,10 @@ function set_routes(server, db_connection) {
                 //  get the value by calling the .value attribute of the object. we can then set the true_password_hash
                 //  that we defined above to that value.
                 request.on('row', columns => {
-                    true_password_hash = columns[0].value;
+                   
+                    true_password = columns[0].value;
+                    console.log('row: ', true_password);
+                    userId = columns[1].value;
                 });
 
                 //  The doneProc event will be evaluated when all of the request functionality is complete.
@@ -213,7 +206,8 @@ function set_routes(server, db_connection) {
                 //  if true is recieved, then the user would be logged in. if false is recieved, the user would be
                 //  told something along the lines of 'username or password incorrect'.
                 request.on('doneProc', function (rowCount, more, returnStatus, rows) {
-                    return resolve(true_password_hash == password_hash);
+                    console.log(true_password, " equals ", password)
+                    return resolve(true_password == password?userId:-1);
                 });
 
                 db_connection.execSql(request);
@@ -234,6 +228,7 @@ function set_routes(server, db_connection) {
                 const request = new Request(
                     `select * from inventory_item`,
                     (err, rowCount) => {
+                        console.log("drinks done");
                         if (err) {
                             console.log(rowCount);
                         } else {
@@ -258,16 +253,56 @@ function set_routes(server, db_connection) {
             });
         }
     });
+    server.route({
+        method: 'GET',
+        path: '/dob/{user_id}',
 
+        //  This fucntion is async so that we can await the database call synchronously
+        handler: async function (request, reply) {
+            //  because we have to wait on the response from the databse, we can call await
+            //  to ensure that we synchronously make our databse call before returning the 
+            //  data to the connection that requested it.
+            const id =request.params.user_id;
+            return await new Promise((resolve, reject) => {
+                const request = new Request(
+                    `select birth_date from users where id = ${id}`,
+                    (err, rowCount) => {
+                      //  console.log("query done");
+                        if (err) {
+                            console.log(rowCount);
+                        } else {
+                            console.log("this worked");
+                            //resolve();
+                        }
+                    }
+                );
+                var dob;
+                request.on('row', columns => {
+                  //  console.log("on");
+                    dob=columns[0];
+                  //  console.log(dob);
+                });
+                request.on('doneProc', function (rowCount, more, returnStatus, rows) {
+                    return resolve(dob);
+                });
+
+                db_connection.execSql(request);
+            });
+        }
+    });
     //Get list of bars by user id
     server.route({
         method: 'GET',
         //Takes in user id as a param through path
         path: '/bars/{user_id}',
         handler: async function (request, reply) {
+            const user_id = request.params.user_id;
+            const query = `select * from bar where address_city = (select address_city from users where id =${user_id})`;
+            console.log(query);
             return await new Promise((resolve, reject) => {
-                const request = new Request(`select * from bars where city = (select city from users where id = '${ user_id }')`,
+                const request = new Request(query,
                     (err, rowCount) => {
+                        console.log(rowCount);
                         if (err) {
                             return resolve({});
                         } else {
@@ -275,7 +310,7 @@ function set_routes(server, db_connection) {
                         }
                     }
                 )
-            });
+           
             //As above, pushes each line of data to an array, then pushes the array to an outer array
             //Outer array is returned as an array of arrays
             var arr = new Array();
@@ -292,21 +327,12 @@ function set_routes(server, db_connection) {
                 return resolve(arr);
             });
             db_connection.execSql(request);
+        });
         }
     });
-
+    
     //Post request test on test table
-    server.route({
-        method: "POST",
-        path: "/book",
-        handler: async (request, resp) => {
-            console.log(":)");
-        }
-        //  This fucntion is async so that we can await the database call synchronously
-        
-    });
 
-    //Post request test on test table
     server.route({
         method: "POST",
         path: "/user",
@@ -335,48 +361,74 @@ function set_routes(server, db_connection) {
             });
         }
     });
-        
-
     server.route({
-            method: "GET",
-            path: "/userdob",
-            handler: async (request, resp) => {
-                //user_id, city, state
-                const userId = parseInt(request.query.id);
-                const dob = request.query.dob;
-               // const state = request.form.state;
-                const update = `UPDATE users SET birth_date = '${dob}' WHERE id = ${userId}`;
-                console.log(update);
-                return new Promise((resolve, reject) => {
-                    //  Create dabase request to count from test table (should be 1)
-                    const request = new Request(update,
-                        (err, rowCount) => {
-                            if (err) {
-                                console.log(err);
-                                resolve(false);
-                            } else {
-                                console.log(rowCount);
-                                resolve(rowCount == 1);
-                            }
+        method: "POST",
+        path: "/updatePassword",
+        handler: async (request, resp) => {
+            //user_id, curr_pass_hash, new_pass_hash  
+            //
+            const userId = parseInt(request.payload.user_id);
+            const currPass = request.payload.curr_pass_hash;
+            const newPass = request.payload.new_pass_hash;
+            const update = `UPDATE users_pass SET password = '${newPass}' WHERE users_id = ${userId} AND password='${currPass}'` ;
+            console.log(update);
+            return new Promise((resolve, reject) => {
+                //  Create dabase request to count from test table (should be 1)
+                const request = new Request(update,
+                    (err, rowCount) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        } else {
+                            console.log(rowCount);
+                            resolve(rowCount == 1);
                         }
-                    );
+                    }
+                );
 
-                    db_connection.execSql(request);
-                });
-
-            }
-        //  This fucntion is async so that we can await the database call synchronously
-
+                db_connection.execSql(request);
+            });
+        }
     });
     server.route({
         method: "GET",
-        path: "/userpw",
+        path: "/changePass",
+        handler: (req, res) => {
+            let c = "Content-Type': 'text/html'";
+
+            c += '<h3>Update:</h3><form action="updatePassword" method="post">';
+            c += 'Use-ID: <input type="text" name="user_id" placeholder="userid"><br/><br/>';
+            c += ' Current Pass: &nbsp;<input type="text" name="curr_pass_hash" placeholder="curr_pass_hash"><br/>';
+            c += ' New Pass: &nbsp;<input type="text" name="new_pass_hash" placeholder="curr_pass_hash"><br/>';
+            c += '<p><input type="submit" value="Update Password"></p>';
+            c += '</form>';
+            return c;
+        }
+    });
+    server.route({
+        method: "GET",
+        path: "/userdob",
+        handler: (req, res) => {
+            let c = "Content-Type': 'text/html'";
+
+            c += '<h3>Update:</h3><form action="updateDOB" method="post">';
+            c += 'Use-ID: <input type="text" name="userId" placeholder="userid"><br/><br/>';
+            c += ' DOB: &nbsp;<input type="text" name="dob" placeholder="dob"><br/>';
+            c += '<p><input type="submit" value="Update Birthdate"></p>';
+            c += '</form>';
+            return c;
+        }
+    });
+
+    server.route({
+        method: "POST",
+        path: "/updateDOB",
         handler: async (request, resp) => {
             //user_id, city, state
-            const userId = parseInt(request.query.id);
-            const pw = request.query.pwd;
+            const userId = parseInt(request.payload.userId);
+            const dob = request.payload.dob;
             // const state = request.form.state;
-            const update = `UPDATE users_pass SET password = '${pw}' WHERE user_id = ${userId}`;
+            const update = `UPDATE users SET birth_date = '${dob}' WHERE id = ${userId}`;
             console.log(update);
             return new Promise((resolve, reject) => {
                 //  Create dabase request to count from test table (should be 1)
@@ -399,6 +451,103 @@ function set_routes(server, db_connection) {
         //  This fucntion is async so that we can await the database call synchronously
 
     });
+
+    server.route({
+        method: "GET",
+        path: "/updateCityState",
+        handler: (req, res) => {
+            let c = "Content-Type': 'text/html'";
+
+            c += '<h3>Update:</h3><form action="updateCityState" method="post">';
+            c += 'User-ID: <input type="text" name="userId" placeholder="User Id"><br/><br/>';
+            c += ' City: &nbsp;<input type="text" name="city" placeholder="City"><br/>';
+            c += ' State: &nbsp;<input type="text" name="state" placeholder="State"><br/>';
+            c += '<p><input type="submit" value="Update User"></p>';
+            c += '</form>';
+            return c;
+        }
+    });
+
+    server.route({
+        method: "POST",
+        path: "/updateCityState",
+        handler: async (request, resp) => {
+            //user_id, city, state
+            const id = parseInt(request.payload.userId);
+            const city = request.payload.city;
+            const state = request.payload.state;
+            // const state = request.form.state;
+            const update = `UPDATE users SET address_city = '${city}', address_state = '${state}' WHERE id = ${id}`;
+            console.log(update);
+            return new Promise((resolve, reject) => {
+                //  Create dabase request to count from test table (should be 1)
+                const request = new Request(update,
+                    (err, rowCount) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        } else {
+                            console.log(rowCount);
+                            resolve(rowCount == 1);
+                        }
+                    }
+                );
+
+                db_connection.execSql(request);
+            });
+
+        }
+        //  This fucntion is async so that we can await the database call synchronously
+
+    });
+
+    server.route({
+        method: "GET",
+        path: "/userpw",
+        handler: (req, res) => {
+            let c = "Content-Type': 'text/html'";
+
+            c += '<h3>Update:</h3><form action="userpw" method="post">';
+            c += 'Username: <input type="text" name="userId" placeholder="userid"><br/><br/>';
+            c += ' Password: &nbsp;<input type="password" name="password" placeholder="password"><br/>';
+            c += '<p><input type="submit" value="Change Password"></p>';
+            c += '</form>';
+            return c;
+        }
+    });
+
+    server.route({
+        method: "POST",
+        path: "/userpw",
+        handler: async (request, resp) => {
+            //user_id, city, state
+            const userId = parseInt(request.payload.userId);
+            const pw = request.payload.password;
+            // const state = request.form.state;
+            const update = `UPDATE users_pass SET password = '${pw}' WHERE users_id = ${userId}`;
+            console.log(update);
+            return new Promise((resolve, reject) => {
+                //  Create dabase request to count from test table (should be 1)
+                const request = new Request(update,
+                    (err, rowCount) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        } else {
+                            console.log(rowCount);
+                            resolve(rowCount == 1);
+                        }
+                    }
+                );
+
+                db_connection.execSql(request);
+            });
+
+        }
+        //  This fucntion is async so that we can await the database call synchronously
+
+    });
+
     //  API Function: do nothing
     //    This route catches all paths that are not explicitly given above.
     //    therefore any call to a URL that isn't defined above will get the
