1b71afa4b70daa754b71030abc5101af4aca0159
anthony mesa <amesa@hyperfix.digital>
2022-03-31 02:58:11 -0400

    update-tab

---
+++ routes/update/updateTab.js
@@ -0,0 +1,35 @@
+
+/**
+ * Comments here
+ * 
+ */
+
+ const { Request } = require("tedious");
+ const { createResponse } = require('../../response')
+ 
+ const updateTab = async (request, db_connection) => {
+ 
+     const tab_id = parseInt(request.payload.tab_id);
+     const bdId = parseInt(request.payload.bar_drink_id);
+ 
+     const update = `insert into tab_drinks (tab_id, bar_drink_id) values (${tab_id}, ${bdId} )`;
+     
+     return new Promise((resolve, reject) => {
+         //  Create dabase request to count from test table (should be 1)
+         const request = new Request(update,
+             (err, rowCount) => {
+                 if (err) {
+                     console.log(err);
+                     resolve(createResponse(-1, null));
+                 } else {
+                     console.log(rowCount);
+                     resolve(createResponse(0, rowCount == 1));
+                 }
+             }
+         );
+ 
+         db_connection.execSql(request);
+     });
+ }
+ 
+ module.exports = { updateReadyStatus: updateTab }
