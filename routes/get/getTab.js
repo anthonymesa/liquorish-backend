8200c93c6b6b5dbe738a964235264838c2c09b12
anthony mesa <amesa@hyperfix.digital>
2022-04-05 23:14:29 -0400

    fixed duplicate tab values showing up

--- routes/get/getTab.js
+++ routes/get/getTab.js
@@ -16,16 +16,22 @@ const getTabDrinks = async (request, db_connection) => {
 
     let tab_drinks = []
 
-    const sql_query = `select drink.drink_id,
-                        drink.drink_name,
-                        drink.description,
-                        bar_drinks.price,
-                        tab_drinks.ready_status
-                        from drink inner join bar_drinks on drink.drink_id = bar_drinks.drink_id 
-                        inner join tabs on tabs.bar_id = bar_drinks.bar_id 
-                        inner join tab_drinks on tabs.tab_id = tab_drinks.tab_id
-                        where tabs.user_id = ${user_id} and bar_drinks.bar_id = ${bar_id}`
-    
+    const sql_query = `    
+    select
+        drink.drink_id,
+        drink.drink_name,
+        drink.description,
+        bar_drinks.price,
+        tab_drinks.ready_status
+    from 
+        (((drink inner join bar_drinks 
+            on drink.drink_id = bar_drinks.drink_id)
+        inner join tabs 
+            on tabs.bar_id = bar_drinks.bar_id)
+        inner join tab_drinks on 
+            tabs.tab_id = tab_drinks.tab_id and 
+            bar_drinks.bar_drink_id = tab_drinks.bar_drink_id)
+    where tabs.user_id = 2 and bar_drinks.bar_id = 2`
 
     const request = new Request(sql_query, (err, rowCount) => {
       if (err) {
