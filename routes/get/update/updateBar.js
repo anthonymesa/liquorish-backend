fa7f7f4aaa8b111d04a533e56739105652f67387
anthonymesa <amesa@hyperfix.digital>
2022-04-21 22:37:57 -0400

    Update updateBar.js

--- routes/get/update/updateBar.js
+++ routes/get/update/updateBar.js
@@ -7,21 +7,22 @@
 const { Request } = require("tedious");
 const { createResponse } = require('../../response')
 
-
 const updateBar = async (request, db_connection) => {
 
-  const barID = parseInt(request.payload.bar_id);
+  const barID = request.payload.bar_id;
   const address = request.payload.address_street;
-    const address_city = request.payload.address_city;
-    const address_state = request.payload.address_state;
-    const address_zip = parseInt(request.payload.address_zip);
-    const owner_pass = request.payload.owner_pass;
-    const update = `UPDATE bar SET 
-                    address_street = '${address_street}',
-                    address_city = '${address_city}',
-                    address_state = '${address_state}',
-                    address_zip = ${address_zip}
-                    WHERE id = ${barID}`;
+  const address_city = request.payload.address_city;
+  const address_state = request.payload.address_state;
+  const address_zip = request.payload.address_zip;
+  const owner_pass = request.payload.owner_pass;
+  
+  const update = `UPDATE bar SET 
+                  address_street = '${address}',
+                  address_city = '${address_city}',
+                  address_state = '${address_state}',
+                  address_zip = ${address_zip}
+                  WHERE id = ${barID}`;
+  
   return new Promise((resolve, reject) => {
     //  Create dabase request to count from test table (should be 1)
     const request = new Request(update,
